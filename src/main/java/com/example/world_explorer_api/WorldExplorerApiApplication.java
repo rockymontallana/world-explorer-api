8eddaf1ab44e64a06865fa0bc7265d1defdb8b7d
rockymontallana <rockymontallana@gmail.com>
2025-11-24 21:12:51 +0800

    initial Spring Boot setup

---
+++ src/main/java/com/example/world_explorer_api/WorldExplorerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.world_explorer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldExplorerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldExplorerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/world_explorer_api/WorldExplorerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.world_explorer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldExplorerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
